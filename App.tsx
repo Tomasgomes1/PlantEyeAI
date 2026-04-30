aa125a9465f7afe45c5679bcd2475b4546f89390
Tomasgomes1 <a416380@alunos.epa.edu.pt>
2026-04-30 10:45:27 +0100

    landing page removida

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import {
   Leaf, Camera, History, Settings, Info, Activity, Zap,
   WifiOff, Wifi, RefreshCw, Map, BarChart2,
 } from 'lucide-react';
-import LandingPage from './components/LandingPage';
 
 import PlantScanner from './components/PlantScanner';
 import LiveAssistant from './components/LiveAssistant';
